a0fef85b9a56956fe4671b417e825cbe94c37ce5
Danakim20 <152463101+Danakim20@users.noreply.github.com>
2024-01-16 12:00:44 +0900

    Java 05_Array_B_Dimension

---
+++ Java/05_Array/src/com/kh/array/B_Dimension.java
@@ -0,0 +1,82 @@
+package com.kh.array;
+
+import java.util.Arrays;
+
+public class B_Dimension {
+
+	/*
+	 * 1. 2차원 배열의 선언
+	 * 
+	 * 		
+	 * 
+	 * 2. 2차원 배열의 생성
+	 * 
+	 * 		배열명 = new 자료형[행크기][열크기];
+	 * 
+	 * 3. 2차원 배열의 선언과 초기화를 동시 진행
+	 * 
+	 * 		자료형[][] 배열명 = new 자료형[행크기][열크기];
+	 * 
+	 * 		배열명[0][0] = 값;
+	 * 		배열명[0][1] = 값;
+	 * 		... 
+	 * */
+	// 선언과 동시에 초기화
+	public void method1() {
+		int[][] arr = {{1, 2, 3, 4, 5},
+						{6, 7, 8, 9, 10},
+						{11, 12, 13, 14, 15}};
+		/*
+		 * [1, 2, 3, 4, 5]
+		 * [6, 7, 8, 9, 10]
+		 * [11, 12, 13, 14, 15]
+		 * */
+		for(int i=0; i<arr.length; i++) {
+			System.out.println(Arrays.toString(arr[i]));
+		}
+		
+		/* 1 ~ 15까지 출력
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr[i].length; j++) {
+				System.out.println(arr[i][j]);
+			}
+		}
+		*/
+		
+		// 향상된 for문!
+		for(int[] i : arr) { // -> arr라는 배열에서 i
+//			System.out.println(Arrays.toString(i));
+			for(int j : i) { // -> i라는 배열에서 j
+				System.out.println(j);
+			}
+		}
+	}
+	
+	// 가변 배열 : 다차원 배열에서 마지막 차수의 크기를 지정하지 않고 다르게 지정
+	/*
+	 * [HTML, CSS, JavaScript]
+	 * [MySQL, Java, JDBC, Servlet, JSP]
+	 * [jQuery, MyBatis, Spring]
+	 * [Spring Boot, React]
+	 * */
+	public void method2() {
+		String[][] arr = {
+							{"HTML", "CSS", "JavaScript"},
+							{"MySQL", "Java", "JDBC", "Servlet", "JSP"},
+							{"jQuery", "MyBatis", "Spring"},
+							{"Spring Boot", "React"}
+						};
+	
+		for(String[] i : arr) {
+			System.out.println(Arrays.toString(i));
+		}
+	}
+	
+	public static void main(String[] args) {
+		B_Dimension d = new B_Dimension();
+//		d.method1();
+		d.method2();
+
+	}
+
+}
