c510b6249e5ac3e56751c6b2f2008dd50f228645
Danakim20 <152463101+Danakim20@users.noreply.github.com>
2024-01-18 17:23:20 +0900

    Java 07_Inheritance_example_practice3_Employee, EmployeeController, Application

---
+++ Java/07_Inheritance/src/com/kh/example/practice3/controller/EmployeeController.java
@@ -0,0 +1,54 @@
+package com.kh.example.practice3.controller;
+
+import com.kh.example.practice3.model.Employee;
+
+public class EmployeeController {
+
+	Employee employee = new Employee();
+	
+	// 오버로딩!
+	
+	// 접근제어자 : public /  : void
+	public void add(int empNo, String name, char gender, String phone) {
+		employee.setEmpNo(empNo);
+		employee.setName(name);
+		employee.setGender(gender);
+		employee.setPhone(phone);
+	}
+	
+	
+	public void add(int empNo, String name, char gender, String phone, String dept, int salary, double bonus) {
+		employee.setEmpNo(empNo);
+		employee.setName(name);
+		employee.setGender(gender);
+		employee.setPhone(phone);
+		employee.setDept(dept);
+		employee.setSalary(salary);
+		employee.setBonus(bonus);
+	}
+	
+	
+	// phone, salary, bonus 따로 지정한 이유 : 한 메서드로 지정 시 안에서 조건을 추가로 걸어야 함
+	// 메서드명은 똑같으나 매개변수나 타입이 다른 경우 : 오버로딩
+	public void modify(String phone) {
+		employee.setPhone(phone);
+	}
+	
+	public void modify(int salary) {
+		employee.setSalary(salary);
+	}
+	
+	public void modify(double bonus) {
+		employee.setBonus(bonus);
+	}
+	
+	
+	public Employee info() {
+		return employee;
+	}
+	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+}
+
+
+
+
