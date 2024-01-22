cc80e3487be31f72ffa075107177691569b4a754
Danakim20 <152463101+Danakim20@users.noreply.github.com>
2024-01-22 17:53:49 +0900

    Java 08_Polymorphism_practice_환경

---
+++ Java/08_Polymorphism/src/com/kh/practice/controller/LibraryController.java
@@ -0,0 +1,20 @@
+package com.kh.practice.controller;
+
+// model 아래 있는 클래스 전부 임포트
+import com.kh.practice.model.*; 
+
+public class LibraryController {
+	
+	private Member member = null;
+	private Book[] bookList = new Book[4];
+	
+	// 인스턴스 초기화 블록 : 객체 생성 시 실행되는 블록
+	/*
+	{
+		bookList[0] = new CookBook("맛있는 지중해식 레시피", true);
+		bookList[1] = new CookBook("카페 샌드위치 마스터 클래스", false);
+		bookList[2] = new AniBook("원피스 107", 19);
+		bookList[3] = new AniBook("주술회전 24", 15); // => 생성자 만들기!
+	}*/
+
+}
