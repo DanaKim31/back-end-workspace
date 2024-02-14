e7de034d75003250f829ae4c723a1660a4bc6a0b
DanaKim31 <danakim731@gmail.com>
2024-02-14 16:49:49 +0900

    Web 03_MemberMVC_풀이

--- Web/03_MemberMVC/src/com/kh/model/dao/MemberDAO.java
+++ Web/03_MemberMVC/src/com/kh/model/dao/MemberDAO.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import com.kh.model.vo.Member;
 
@@ -53,6 +54,42 @@ public class MemberDAO {
 		close(ps, conn);
 		
 		return result;
+	}
+	
+	public ArrayList<Member> showAllMember() throws SQLException {
+		Connection conn = getConnect();
+		
+		String query = "SELECT * FROM member";
+		PreparedStatement ps = conn.prepareStatement(query);
+		
+		ArrayList<Member> list = new ArrayList<>();
+		ResultSet rs = ps.executeQuery();
+		while(rs.next()) {
+			list.add(new Member(rs.getString("id"), 
+								rs.getString("password"),
+								rs.getString("name")));
+		}
+		close(rs, ps, conn);
+		return list;
+	}
+	
+	public Member searchMember(String name) throws SQLException {
+		Connection conn = getConnect();
+		
+		String query = "SELECT * FROM member WHERE name=?";
+		PreparedStatement ps = conn.prepareStatement(query);
+		ps.setString(1, name);
+		
+		ResultSet rs = ps.executeQuery();
+		Member member = null;
+		if(rs.next()) {
+			member = new Member(
+					rs.getString("id"), 
+					rs.getString("password"), 
+					rs.getString("name"));
+		}
+		close(rs, ps, conn);
+		return member;
 	}
 
 }
